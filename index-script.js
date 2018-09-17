660930bcf67ae0bb33e61b4139302299421de5bb
Nick Noel-Labieniec <nicknoel251289@gmail.com>
2018-09-16 20:10:32 -0700

    First commit

---
+++ index-script.js
@@ -0,0 +1,58 @@
+document.onreadystatechange = function () {
+    if (document.readyState == "interactive") {
+
+
+      var hamburger = document.getElementById('hamburger');
+      var hamburgLine = document.getElementById('line');
+      var navTop = document.getElementById('top-nav');
+      var main = document.getElementById('main');
+
+      function openTopNav(){
+        if(window.innerWidth > 736){
+          navTop.style.width = "300px";
+          main.style.marginRight = "300px";
+          main.style.transitionDuration = ".5s";
+        }else if(window.innerWidth < 736){
+          navTop.style.width = "100%";
+          main.style.display = "block";
+          main.style.transitionDuration = ".5s";
+        }
+      }
+      function closeTopNav(){
+        navTop.style.width = "0";
+        main.style.marginRight = "0";
+      }
+
+      hamburger.addEventListener("click", function(){
+        hamburger.classList.toggle('active');
+        if(hamburger.classList.contains('active')){
+          openTopNav();
+        }else{
+          closeTopNav();
+        }
+      });
+
+      window.addEventListener('resize', function(){
+        if(window.innerWidth > 950){
+          navTop.style.width = "auto";
+          if(hamburger.classList.contains('active')){   //if hamburg menu has been clicked and shows an X
+            hamburger.classList.toggle('active');       //when resizing less than 950 it will still be toggled open and show
+            main.style.marginRight = "0";               //want to reset the tag/element of main that was push 300px
+          }
+        }else{//else if window.innerWidth < 950
+          if(!hamburger.classList.contains('active')){
+            navTop.style.width = "0"; //the nav is set to auto (above) on window resize > 950
+                                      //this resets its width to 0 and hides it for when < 950
+                                      //otherwise you see the nav because its still set to atuo from above
+          }else if(hamburger.classList.contains('active') && window.innerWidth < 736){
+            openTopNav();
+          }else if(hamburger.classList.contains('active') && window.innerWidth > 736 && window.innerWidth < 950){
+            navTop.style.width = "300px";
+            main.style.marginRight = "300px";
+            main.style.transitionDuration = ".5s";
+          }
+        }
+      });
+
+    }
+}
